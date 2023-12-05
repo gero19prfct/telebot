93f73e3ffadb41d20f9bc436b86f1e50ab87e1ab
aidoskz <aidos_2505@inbox.ru>
2023-12-05 16:17:36 +0600

    fix messages and a new token

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 
-TOKEN = '6934660504:AAG0YVy2sFNgkQKBrRYkH7bsYuU8JZLK82U'
+TOKEN = '6963951167:AAE-RQUnCuJGOan8OZCQ4tICBN3xWya8F5o'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
@@ -10,9 +10,9 @@ def welcome(message):
 @bot.message_handler(content_types=['text'])
 def talk(message):
     if(message.text.isnumeric() and len(message.text)==12):
-        bot.send_message(message.chat.id,"Успешно сделано")
+        bot.send_message(message.chat.id,"Правильно, теперь на этот каспий номер +7 776 221 9957 Илияс Н")
     if(message.text.isnumeric() and (len(message.text)>12 or len(message.text)<12)) :
-        bot.send_message(message.chat.id,"не правилно")
+        bot.send_message(message.chat.id,"не правилно напишите свой иин правильно")
     if(message.text.isnumeric()==False):
         bot.send_message(message.chat.id,"Напишите ИИН")    
     
